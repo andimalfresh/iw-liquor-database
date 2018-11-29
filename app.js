f3a53035e161216257d928a7c231532d8c648256
unknown <w.andrew.pedersen@gmail.com>
2018-11-29 16:51:28 -0700

    Added Routes

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ app.listen(port, console.log(`Telegraphing on ${port}...into the heavens...`));
 app.get("/", (req, res) => {
   queries.listAll().then(results => res.send(results));
 });
+
+app.get("/:id", (req, res) => {
+  queries.getById(req.params.id).then(results => res.send(results));
+});
+app.delete("/:id", (req, res) => {
+  queries.deleteLiquor(req.params.id).then(res.sendStatus(204));
+});
+app.post("/", (req, res) => {
+  queries.createLiquor(req.body).then(res.sendStatus(201));
+});
+app.put("/:id", (req, res) => {
+  queries
+    .updateLiquor(req.params.id, req.body)
+    .then(liquor => res.json(liquor));
+});
--- queries.js
+++ queries.js
@@ -3,5 +3,24 @@ const database = require("./database-connection");
 module.exports = {
   listAll() {
     return database("spirits-list");
+  },
+  getById(id) {
+    return database("spirits-list").where("id", id);
+  },
+  deleteLiquor(id) {
+    return database("spirits-list")
+      .where("id", id)
+      .delete();
+  },
+  createLiquor(pushLiquor) {
+    return database("spirits-list")
+      .insert(pushLiquor)
+      .returning("*");
+  },
+  updateLiquor(id, liquor) {
+    return database("spirits-list")
+      .where("id", id)
+      .update(liquor)
+      .returning("*");
   }
 };
