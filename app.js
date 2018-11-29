035e03e4aaccf733f9453ccec8fa7f9a43c06cee
unknown <w.andrew.pedersen@gmail.com>
2018-11-29 11:02:46 -0700

    Add first Route

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const app = express();
 const port = process.env.PORT || 6661;
 
 app.listen(port, console.log(`Telegraphing on ${port}... Hi Carla!`));
+
+app.get("/", (req, res) => {
+  res.send("Be sure when you make the DataBase to add Carla in it");
+});
